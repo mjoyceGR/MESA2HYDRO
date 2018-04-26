04a1051e9d85c755380ca63c2c5eff6dfbfb114d
Lianne Lairmore <lianne.lairmore@gmail.com>
2018-04-26 02:20:29 -0400

    Fix path for meridith

--- work/alg.py
+++ work/alg.py
@@ -30,7 +30,7 @@ try_reload=True
 format_type='binary' 
 
 
-MESA_file='out/sample_MESA_output/profile140.data'
+MESA_file='../out/sample_MESA_output/profile140.data'
 masscut=0.95
 N=32
 mp=1e-7 ##IN UNITES OF Msolar!!!
