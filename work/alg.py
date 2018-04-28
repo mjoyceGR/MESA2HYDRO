9c2608744f004c434e1a866e4c7376f1a2512d50
Lianne Lairmore <lianne.lairmore@gmail.com>
2018-04-28 16:40:39 -0400

    Adds command line arguments and installs pygadgetic correctly

--- work/alg.py
+++ work/alg.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -19,29 +20,99 @@ start_time = time.time()
 # R_to_solar=6.957*10.0**10.0 ## cm/Rsolar
 # ###################################################
 
+def path_from_package(path):
+    return os.path.join(MESA_PKG_DIR, path)
+
 #######################################################
 #
 # user controls
 #
 ########################################################
-check_MESA=False
-make_NR_file=False
-make_IC_file=True
-try_reload=True
-format_type='binary' 
+def defaults():
+    check_MESA=False
+    make_NR_file=False
+    make_IC_file=True
+    try_reload=True
+    format_type='binary' 
 
 
-MESA_file=os.path.join(MESA_PKG_DIR, 'out/sample_MESA_output/profile140.data')
-masscut=0.95
-N=32
-mp=1e-7 ##IN UNITES OF Msolar!!!
+    MESA_file=os.path.join(MESA_PKG_DIR, 'out/sample_MESA_output/profile140.data')
+    masscut=0.95
+    N=32
+    mp=1e-7 ##IN UNITES OF Msolar!!!
 
-startype='p140_test'
-tag=startype+'_m'+str(masscut)+'_N'+str(N)+'_'+'mp'+str(mp)
-outname=tag
+    startype='p140_test'
+    tag=startype+'_m'+str(masscut)+'_N'+str(N)+'_'+'mp'+str(mp)
+    outname=tag
 
-saveNR="saveNR_"+startype+".dat"
+    saveNR=os.path.join(MESA_PKG_DIR, "work/NR_files/saveNR_ms.dat")
 
+
+##############################################################
+#
+# Argument input
+#
+##############################################################
+parser = argparse.ArgumentParser(description='')
+input_types = parser.add_mutually_exclusive_group()
+input_types.add_argument('--config-file', type=file,
+                         help='Path to configuration file')
+config_args = input_types.add_argument_group()
+config_args.add_argument('--check-MESA', action='store_true',
+                         help='Sets check-MESA value')
+config_args.add_argument('--make-NR-file', action='store_true',
+                         help='Sets make-NR-file')
+config_args.add_argument('--no-IC-file', action='store_true',
+                         help='Sets make-IC-file value')
+config_args.add_argument('--no-reload', action='store_true',
+                         help='Sets try-reload')
+config_args.add_argument('--format-type', default='binary',
+                         help='Type of the format (binary...)')
+config_args.add_argument('--MESA-file',
+                         default=path_from_package('out/sample_MESA_output/profile140.data'),
+                         help='Path to input MESA output')
+config_args.add_argument('--masscut', default=0.95,
+                         help='Sets masscut')
+config_args.add_argument('--N', default=32,
+                         help='Sets N')
+config_args.add_argument('--mp', default=1e-7,
+                         help='Set the mp value in Msolar units')
+config_args.add_argument('--start-type', default='p140_test',
+                         help='Set start type')
+config_args.add_argument('--saveNR', default=None,
+                         help='Set the saveNR file')
+
+args = parser.parse_args()
+
+if args.config_file:
+    # TODO: Read configurations from a file
+    pass
+else:
+    check_MESA = args.check_MESA
+    make_NR_file = args.make_NR_file
+    make_IC_file = not args.no_IC_file
+    try_reload = not args.no_reload
+    format_type = args.format_type
+    MESA_file = args.MESA_file
+    masscut = args.masscut
+    N = args.N
+    mp = args.mp
+    startype = args.start_type
+    tag = startype + '_m' + str(masscut) + '_N' + str(N) + '_' + 'mp' + str(mp)
+    outname = tag
+    # If a saveNR path is given use that
+    # otherwise construct one from other variables
+    if args.saveNR:
+       saveNR = args.saveNR
+    else: 
+        saveNR = "saveNR_"+startype+".dat"
+    # If the given path exists use that otherwise
+    # prepend the package path
+    if not os.path.exists(saveNR):
+        saveNR = path_from_package(saveNR)
+        
+        
+    
 
 ##############################################################
 #
