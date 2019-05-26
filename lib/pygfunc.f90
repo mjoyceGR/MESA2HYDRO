1751bbc6bf8a0171e94c6249c98c748549e15371
Lianne Lairmore <lianne.lairmore@gmail.com>
2019-05-26 18:55:17 -0400

    Liannes' changes to get pyx file to work

--- lib/pygfunc.f90
+++ lib/pygfunc.f90
@@ -1,13 +1,12 @@
 module write_data_phantom_interface
 use iso_c_binding, only: c_double, c_int  !magic?
 use phantread_module, only: write_data_phantom
-implicit none
+!implicit none
 contains
 
 !! pass MY python things here, through this, and build daniel's things out of them
 
 subroutine c_gfunc(ngas,mgas,x,y,z,h,u,msink) bind(c)!!x, n, m, a, b, c 
-	
 	!!int* ngas,int* mgas,double* x,double* y,double* z, double*h, double* u,double* msink
 
 	!! ACCEPTS THINGS FROM PYTHON
@@ -20,46 +19,57 @@ subroutine c_gfunc(ngas,mgas,x,y,z,h,u,msink) bind(c)!!x, n, m, a, b, c
     ! real(c_double), dimension(m), intent(in) :: b
     ! real(c_double), dimension(n, m), intent(out) :: c
 
-    integer(c_int), intent(in) :: nsink, ntypes, npartoftype, massoftype
-    integer(c_int), intent(in) :: ngas, mgas !! number of particles
-    real(c_double), intent(in) :: x, y, z, h, u, msink
+    !integer(c_int), intent(in) :: nsink, ntypes, npartoftype, massoftype
+    integer(c_int), intent(in) :: ngas
+    integer(c_int), intent(in) :: mgas(ngas)
+    real(c_double), intent(in) :: x(ngas), y(ngas), z(ngas), h(ngas), u(ngas), msink(ngas)
+
+    print *, ngas
+    print *, mgas
+    print *, x
+    print *, y
+    print *, z
+    print *, h
+    print *, u
+    print *, msink
 
     !n = ngas
 
-    real, allocatable :: dat(:,:), datsink(:,:)
+!    real, allocatable :: dat(:,:), datsink(:,:)
 
-    nsink = 1
-    allocate(dat(8,n),datsink(6,nsink))
+!    nsink = 1
+!    allocate(dat(8,n),datsink(6,nsink))
     !! BUILD the dat, datsink, label, and label_sink data structures in the form compatible with phantom reader
 
 
-    dat(1,:) = x(:)
-    dat(2,:) = y(:)
-    dat(3,:) = z(:)
-    dat(4,:) = h(:)
-    dat(5,:) = 0. !vx(:)
-    dat(6,:) = 0. !vy(:)
-    dat(7,:) = 0. !vz(:)
-    dat(8,:) = u(:)
+!    dat(1,:) = x
+!    dat(2,:) = y
+!    dat(3,:) = z
+!    dat(4,:) = h
+!    dat(5,:) = 0. !vx(:)
+!    dat(6,:) = 0. !vy(:)
+!    dat(7,:) = 0. !vz(:)
+!    dat(8,:) = u
 
-    label = (/'x','y','z','h','u'/)
+    !label = (/'x','y','z','h','u'/)
 
-    datsink(1,1) = xsink
-    datsink(2,1) = ysink
-    datsink(3,1) = zsink
-    datsink(4,1) = msink
-    datsink(5,1) = rsink
-    datsink(6,1) = hsoftsink
+!    datsink(1,1) = xsink
+!    datsink(2,1) = ysink
+!    datsink(3,1) = zsink
+!    datsink(4,1) = msink
+!    datsink(5,1) = rsink
+!    datsink(6,1) = hsoftsink
 
-    label_sink = (/'x','y','z','m','h','hsoft'/)
+!    label_sink = (/'x','y','z','m','h','hsoft'/)
 
-    ntypes = 1
-    npartoftype(1) = ngas
-    massoftype(1) = mgas
+!    ntypes = 1
+    !npartoftype(1) = ngas
+    !massoftype(1) = mgas
 
-    call write_data_phantom(n,dat,label,datsink,label_sink)
+!    call write_data_phantom(n,dat,label,datsink,label_sink)
+    print *, "Hello?"
 
-    deallocate(dat,datsink)
+!    deallocate(dat,datsink)
 
 end subroutine
 end module
--- lib/pygfunc.h
+++ lib/pygfunc.h
@@ -1,6 +1,6 @@
 /*extern void c_gfunc(int* n);
 */
-extern void c_gfunc(int* ngas,int* mgas,double* x,double* y,double* z, double*h, double* u,double* msink);
+extern void c_gfunc(int ngas, int* mgas, double* x, double* y, double* z, double* h, double* u, double* msink);
 
 /* whatever the arguments in pygfunc.f90 are, in here
 ex) extern void c_gfunc(double* x, int* n, int* m, double* a, double* b, double* c);
